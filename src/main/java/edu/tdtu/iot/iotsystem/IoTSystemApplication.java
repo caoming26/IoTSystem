964c11260e9cfaa9f7c43a4ea333ce9281e18661
caoming26 <caominh26032003@gmail.com>
2024-10-28 11:25:58 +0700

    v0

---
+++ src/main/java/edu/tdtu/iot/iotsystem/IoTSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.tdtu.iot.iotsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tdtu/iot/iotsystem/IoTSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tdtu.iot.iotsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
